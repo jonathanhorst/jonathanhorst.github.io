d555944405762df637078d7d49712877b79d63c2
jonathanhorst <jonathan@jonathan.ht>
2018-05-29 18:25:46 -0500

    1.0

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -1,1 +1,29 @@
 // @codekit-prepend "./Menu"
+$(document).ready(function() {
+
+
+  // Add class while scrolling
+  // https://jsfiddle.net/Mottie/4C5KW/
+  $(function() {
+
+    var timer, el = $('body'),
+      flag = false;
+    $(window).scroll(function() {
+      if (!flag) {
+        flag = true;
+        el.addClass('is-scrolling');
+      }
+      clearTimeout(timer);
+      timer = setTimeout(function() {
+        el.removeClass('is-scrolling');
+        flag = false;
+      }, 100);
+    });
+
+  });
+
+
+});
+
+// Initiate lazy load
+lazyload();
